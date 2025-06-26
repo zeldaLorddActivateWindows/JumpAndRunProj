9226dc60722958068bb70590fdb10957e017094a
Josef Lackerbauer <j.lackerbauer@htlvb.at>
2025-06-26 12:09:33 +0200

    implement powerup.cs

--- src/Powerup.cs
+++ src/Powerup.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using JumpAndRun;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace main.src
+namespace JumpAndRun
 {
+    record struct Point
+    {
+        public int X;
+        public int Y;
+        public Point(int x, int y) { X = x; Y = y; }
+    }
     internal class Powerup
     {
+        Point Position { get; set; }
+        string Name {  get; set; }
+        float DoubleSpeed {  get; set; } 
+        float Dash {  get; set; }
+        bool IsDashing { get; set; } = false;
+
+        public Powerup()
+        {
+
+        }
+
     }
 }
