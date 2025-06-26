6c5626c8b1910da29235a4f3415003cb856cd82a
zeldalorddactivatewindows <benjaminstockhamer@gmail.com>
2025-06-26 10:36:43 +0200

    add controls and collision handling

--- src/Player.cs
+++ src/Player.cs
@@ -9,21 +9,73 @@ namespace JumpAndRun
 {
     internal class Player
     {
-        string Name { get; set; }
-        float Score { get; set; } = 0;
-        float XVelocity { get; set; } = 10;
-        int JumpHeight { get; set; } = 10;
-        Vector2 Position { get; set; } = new Vector2(0, 0);
+        public string Name { get; set; }
+        public float Score { get; set; } = 0;
+        public float XVelocity { get; set; } = 200; 
+        public float YVelocity { get; set; } = 0;
+        public float JumpStrength { get; set; } = 400; 
+        public Vector2 Position { get; set; } = new Vector2(100, 400);
         public bool IsGrounded { get; set; } = false;
         public bool CanDoubleJump { get; set; } = true;
+        public int Width { get; set; } = 50;
+        public int Height { get; set; } = 50;
 
-        public Player(string eName) 
+        private const float Gravity = 980; 
+        private const float GroundY = 500; 
+        private bool hasDoubleJumped = false;
+
+        public Player(string playerName)
         {
-            eName = Name;
-            
+            Name = playerName;
         }
 
+        public void Update(float deltaTime)
+        {
+            HandleInput();
+            if (!IsGrounded) YVelocity += Gravity * deltaTime;
+            Position = new Vector2(Position.X, Position.Y + YVelocity * deltaTime);
+            if (Position.Y >= GroundY - Height)
+            {
+                Position = new Vector2(Position.X, GroundY - Height);
+                YVelocity = 0;
+                IsGrounded = true;
+                hasDoubleJumped = false;
+            }
+            else IsGrounded = false;
+            
 
+            if (Position.X < 0) Position = new Vector2(0, Position.Y);
+            else if (Position.X > 800 - Width) Position = new Vector2(800 - Width, Position.Y);
+        }
 
+        private void HandleInput()
+        {
+            float deltaTime = Raylib_cs.Raylib.GetFrameTime();
+
+            if (Raylib_cs.Raylib.IsKeyDown(Raylib_cs.KeyboardKey.A) || Raylib_cs.Raylib.IsKeyDown(Raylib_cs.KeyboardKey.Left)) Position = new Vector2(Position.X - XVelocity * deltaTime, Position.Y);
+            if (Raylib_cs.Raylib.IsKeyDown(Raylib_cs.KeyboardKey.D) || Raylib_cs.Raylib.IsKeyDown(Raylib_cs.KeyboardKey.Right)) Position = new Vector2(Position.X + XVelocity * deltaTime, Position.Y);
+            if (Raylib_cs.Raylib.IsKeyPressed(Raylib_cs.KeyboardKey.Space) || Raylib_cs.Raylib.IsKeyPressed(Raylib_cs.KeyboardKey.W) || Raylib_cs.Raylib.IsKeyPressed(Raylib_cs.KeyboardKey.Up))
+            {
+                if (IsGrounded)
+                {
+                    YVelocity = -JumpStrength;
+                    IsGrounded = false;
+                }
+                else if (CanDoubleJump && !hasDoubleJumped)
+                {
+                    YVelocity = -JumpStrength;
+                    hasDoubleJumped = true;
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            Raylib_cs.Raylib.DrawRectangle((int)Position.X, (int)Position.Y, Width, Height, Raylib_cs.Color.Blue);
+            Raylib_cs.Raylib.DrawCircle((int)Position.X + 12, (int)Position.Y + 15, 3, Raylib_cs.Color.White);
+            Raylib_cs.Raylib.DrawCircle((int)Position.X + 38, (int)Position.Y + 15, 3, Raylib_cs.Color.White);
+            Raylib_cs.Raylib.DrawText(Name, (int)Position.X, (int)Position.Y - 20, 12, Raylib_cs.Color.Black);
+        }
     }
 }
+
--- src/main.cs
+++ src/main.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JumpAndRun
+﻿namespace JumpAndRun
 {
     using Raylib_cs;
     using System.Runtime.InteropServices;
-
+    
     internal class main : IGameWindow
     {
         public string NAME { get; } = "Jump and Run";
@@ -16,22 +10,17 @@ namespace JumpAndRun
         public int HEIGHT { get; } = 600;
         public int FPS { get; } = 60;
         private bool isRunning = true;
-        private List<KeyboardKey> ExitKeys = new List<KeyboardKey>()
-        {
-            KeyboardKey.Escape, KeyboardKey.Enter
-        };
-
+        private List<KeyboardKey> ExitKeys = new List<KeyboardKey>() { KeyboardKey.Escape, KeyboardKey.Enter };
+        private Player player;
         internal static void Main(string[] args)
         {
             main game = new main();
             game.Init();
-
+            
             while (!Raylib.WindowShouldClose() && game.isRunning)
             {
                 game.Update();
-                Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.RayWhite);
-                Raylib.EndDrawing();
+                game.Draw();
             }
             game.Exit();
         }
@@ -40,6 +29,7 @@ namespace JumpAndRun
         {
             Raylib.InitWindow(WIDTH, HEIGHT, NAME);
             Raylib.SetTargetFPS(FPS);
+            player = new Player("pexlover");
         }
 
         public void Update()
@@ -52,8 +42,31 @@ namespace JumpAndRun
                     break;
                 }
             }
+            float deltaTime = Raylib.GetFrameTime();
+            player.Update(deltaTime);
         }
-        public void Exit() => Raylib.CloseWindow();
+
+        public void Draw()
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.SkyBlue);
+            Raylib.DrawRectangle(0, 500, WIDTH, HEIGHT - 500, Color.Green);
+            player.Draw();
+            Raylib.DrawText("Controls:", 10, 10, 16, Color.Black);
+            Raylib.DrawText("A/D or Arrow Keys to move", 10, 30, 12, Color.Black);
+            Raylib.DrawText("Space to Jump", 10, 45, 12, Color.Black);
+            Raylib.DrawText("ESC or ENTER to Exit", 10, 60, 12, Color.Black);
+            
+            string groundedText = player.IsGrounded ? "Grounded" : "Airborne";
+            string doubleJumpText = player.CanDoubleJump ? "Double Jump Available" : "Double Jump Used";
+            Raylib.DrawText($"Status: {groundedText}", 10, HEIGHT - 60, 12, Color.Black);
+            Raylib.DrawText($"{doubleJumpText}", 10, HEIGHT - 45, 12, Color.Black);
+            Raylib.DrawText($"Score: {player.Score}", 10, HEIGHT - 30, 12, Color.Black);
+            
+            Raylib.EndDrawing();
+        }
         
+        public void Exit() => Raylib.CloseWindow();
     }
 }
+
