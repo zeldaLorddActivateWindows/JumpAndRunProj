62083e285ab6269df83bc3630f051a6edc623a5a
Josef Lackerbauer <j.lackerbauer@htlvb.at>
2025-06-26 10:10:51 +0200

    implement palyerclass

---
+++ src/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpAndRun
+{
+    internal class Player
+    {
+        string Name { get; set; } = string.Empty;
+        float Score { get; set; } = 0;
+        float XVelocity { get; set; } = 10;
+        int JumpHeight { get; set; } = 10;
+        Vector2 Position { get; set; } = new Vector2(0, 0);
+        public bool IsGrounded { get; set; } = false;
+        public bool CanDoubleJump { get; set; } = true;
+
+        public Player(string eName, float eScore) 
+        {
+            eName = Name;
+            eScore = Score;
+            
+        }
+
+
+
+    }
+}
